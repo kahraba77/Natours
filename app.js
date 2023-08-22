589f5327e8fc81eebbc8ac236c0ca5b504f70678
kahraba77 <88279972+kahraba77@users.noreply.github.com>
2023-08-22 05:19:25 +0300

    0.1

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -23,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(compression());
 // Set security HTTP headers
 app.use(helmet());
 
